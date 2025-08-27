6a3d9137ef291a9e907fc7a1cc1ec0f9d0f01fa9
Alexandr Sekera <sekera@arion.cz>
2025-08-27 15:43:13 +0200

    CLI startup and arguments display

--- FolderSync/Program.cs
+++ FolderSync/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("FolderSync started.");
+Console.WriteLine("Arguments received:");
+foreach (string arg in args)
+{
+    Console.WriteLine(arg);
+}
+Console.ReadKey();
