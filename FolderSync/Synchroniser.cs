d83de13cefa4ca96a3aa4640616107521f6e9e3b
Alexandr Sekera <sekera@arion.cz>
2025-08-28 08:39:27 +0200

    copying files (no deleting)

--- FolderSync/Synchroniser.cs
+++ FolderSync/Synchroniser.cs
@@ -10,16 +10,16 @@ public class Synchroniser
     private readonly CancellationToken cancellationToken;
 
     public Synchroniser(SyncConfig config, Logger logger, CancellationToken cancellationToken)
-	{
+    {
         this.config = config;
         this.logger = logger;
         this.cancellationToken = cancellationToken;
-	}
+    }
 
     /// <summary>
     /// Starts the main synchronisation loop.
     /// </summary>
-    public void Start() 
+    public void Start()
     {
         while (!cancellationToken.IsCancellationRequested)
         {
@@ -39,8 +39,50 @@ public class Synchroniser
     /// <summary>
     /// Synchronises source and replica directories.
     /// </summary>
-    private void Synchronise() 
+    private void Synchronise()
     {
-        logger.Log(LogLevel.INF, "Synchronising...");
+        // Check if a file from source folder exists in the replica folder
+        // If the file doesn't exist, copy it
+        // If the file exists, check the last write time
+        // If it's the same, ignore -> otherwise copy
+        // All subfolders are handled the same way
+
+        // Check if a file from replica folder exists in the source folder
+        // If the file exists, ignore it -> otherwise delete it
+        // All subfolders are handled the same way
+
+        logger.Log(LogLevel.INF, "Starting directory synchronisation...");
+        SyncDir();
+        RemoveExtras();
+        logger.Log(LogLevel.INF, "Directory synchronization completed.");
+    }
+
+    private void SyncDir() 
+    {
+        string fileName = string.Empty;
+        string replicaFile = string.Empty;
+        string dirName = string.Empty;
+        string replicaSubDir = string.Empty;
+        bool shouldCopy = false;
+
+        foreach (var file in Directory.GetFiles(config.SourcePath))
+        {
+            fileName = Path.GetFileName(file);
+            replicaFile = Path.Combine(config.ReplicaPath, fileName);
+            shouldCopy = !File.Exists(replicaFile) ||
+                File.GetLastWriteTimeUtc(file) != File.GetLastWriteTimeUtc(replicaFile);
+
+            if (shouldCopy)
+            {
+                File.Copy(file, replicaFile, true);
+                logger.Log(LogLevel.INF, $"Updated file: {fileName}");
+            }
+            else logger.Log(LogLevel.WAR, $"Skipped file: {fileName}");
+        }
+    }
+
+    private void RemoveExtras() 
+    {
+
     }
 }
